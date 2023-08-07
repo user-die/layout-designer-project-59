87d9f820c6e88f5b08c5e6f1a8f7a89dc5c73f76
userdie <vadim.vadimov123@mail.ru>
2023-08-08 00:32:02 +0500

    1

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const { src, dest, parallel, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const pug = require("gulp-pug");
+const browserSync = require("browser-sync").create();
+
+const browserSyncJob = () => {
+  browserSync.init({
+    server: "build/",
+  });
+
+  watch(["src/sass/.scss"], buildSass);
+  watch(["src/pages/.pug"], buildPug);
+};
+
+const buildSass = () => {
+  console.log("Компиляция SASS");
+
+  return src("src/sass/.scss")
+    .pipe(sass())
+    .pipe(dest("build/styles/"))
+    .pipe(browserSync.stream());
+};
+
+const buildPug = () => {
+  console.log("Компиляция Pug");
+
+  return src("src/pages/.pug")
+    .pipe(pug())
+    .pipe(dest("build/"))
+    .pipe(browserSync.stream());
+};
+
+exports.server = browserSyncJob;
+exports.build = parallel(buildSass, buildPug);
+g;
